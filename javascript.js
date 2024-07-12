6255e8ffd08703c760bd11d4bc5092ba787d015b
Aaron Urrea <aaronurrea343@gmail.com>
2024-07-11 19:21:45 -0700

    UI WOW

--- javascript.js
+++ javascript.js
@@ -1,78 +1,58 @@
 let humanScore = 0, computerScore = 0;
-
+const buttons = document.querySelectorAll("button");
 const dict = {
     'rock' : 2,
     'paper' : 1,
     'scissors' : 0,
 };
 
-playGame();
 
+buttons.forEach((button) => {
+    button.addEventListener("click", function() {
+        playRound(button.textContent.toLowerCase());})});
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
-    console.log("Computer: " + Object.keys(dict).find(key => dict[key] === choice));
+    
+    document.querySelector('#cChoice').textContent=Object.keys(dict).find(key => dict[key] === choice);;
     return choice;
 }
 
-function getHumanChoice(){
 
-    let choice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    console.log("Human: " + choice);
-    if (dict[choice] === undefined){
-        console.log("That is not a correct choice");
-        return;
-    }   
-
-    return dict[choice];
-
-
-}
-
-function playRound(round){
-    console.log("Current round: " + round);
-    let humanChoice;
-    while (humanChoice === undefined){
-        humanChoice = getHumanChoice();
-    }
+function playRound(choice){
+    document.querySelector('#hChoice').textContent=choice;
+    let humanChoice = dict[choice];
 
     let computerChoice = getComputerChoice();
 
     let result = humanChoice - computerChoice;
+
     console.log();
     switch(result){
         case 1:
         case -2:
             computerScore++;
-            console.log("Computer won! Current score: " + humanScore + " to " + computerScore);
+            document.querySelector("#cScore").textContent = computerScore;
+            document.querySelector('#result').textContent = "Computer win this round!";
+
             break;
         case 0:
-            console.log("It's a tie! Trying again");
-            playRound(round);
+            document.querySelector('#result').textContent = "It's a tie!";
             break;
         case 2:
         case -1:
             humanScore++;
-            console.log("Human won! Current score: " + humanScore + " to " + computerScore);
+            document.querySelector("#hScore").textContent = humanScore;
+            document.querySelector('#result').textContent = "You win this round!";
             break;
     }
-}
+    if(humanScore === 5){
+        document.querySelector('#result').textContent = "You win the game!";
 
-function playGame(){
-    for(let i = 1; i <=5; i++){
-        playRound(i);
-        console.log();
     }
+    else if(computerScore === 5){
+        document.querySelector('#result').textContent = "Computer wins the game!";
 
-    console.log("Final scores: " + humanScore + " to " + computerScore);
-
-    if(computerScore > humanScore)
-        console.log("Computer won!");
-    else if(computerScore === humanScore){
-        console.log("It's a tie! (HOW?)");
-    }
-    else{
-        console.log("Human won!");
     }
 }
 
